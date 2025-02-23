99decc845e814482c9a778a8f4f2f69c26f00cb5
SingHarshit <145246015+SingHarshit@users.noreply.github.com>
2025-02-23 18:10:50 +0530

    change origin

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const nodemailer = require("nodemailer"); // ✅ Import Nodemailer
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true })); // ✅ More secure CORS settings
+app.use(cors({ origin: "*", credentials: true })); // ✅ More secure CORS settings
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
